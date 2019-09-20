e063bc6ee393f6bc0e797330d63e77bc8417259c
gabrielpelmus <gabriel.pelmus@gmail.com>
2019-09-20 20:26:17 +0300

    updates to class

--- WeatherApp/scripts/forecast.js
+++ WeatherApp/scripts/forecast.js
@@ -1,22 +1,32 @@
-const key = 'DsFs9QfJB3KaArtBaAxLQPYxrCc6OrWS';
+class Forecast{
+    constructor(){
+        this.key = 'DsFs9QfJB3KaArtBaAxLQPYxrCc6OrWS';
+        this.baseCityUri = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+        this.baseWeatherUri = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    }
 
-const getWeather = async (id) => {
-    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
-    const query = `${id}?apikey=${key}`;
+    async updateCity(city){
+        const cityDets = await this.getCity(city);
+        const weather = await this.getWeather(cityDets.Key);
 
-    const response = await fetch(base + query);
-    const data = await response.json();
+        return { cityDets, weather };
+    }
 
-    return data[0];
+    async getCity(city){
+        const query = `?apikey=${this.key}&q=${city}`;
+
+        const response = await fetch(this.baseCityUri + query);
+        const data = await response.json();
+
+        return data[0];
+    }
+
+    async getWeather(id){
+        const query = `${id}?apikey=${this.key}`;
+
+        const response = await fetch(this.baseWeatherUri + query);
+        const data = await response.json();
+
+        return data[0];
+    }
 }
-
-const getCity = async (city) => {
-    
-    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
-    const query = `?apikey=${key}&q=${city}`;
-
-    const response = await fetch(base + query);
-    const data = await response.json();
-
-    return data[0];
-};
