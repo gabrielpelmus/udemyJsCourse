5edd49cad2e626fc673719f63203d55cfab513ed
Gabriel <gabriel.pelmus@gmail.com>
2019-12-12 14:44:10 +0200

    Section 4

--- asp/DatingApp/DatingApp.API/Controllers/ValuesController.cs
+++ asp/DatingApp/DatingApp.API/Controllers/ValuesController.cs
@@ -22,6 +22,7 @@ namespace DatinApp.API.Controllers
         }
 
         // HttpGetAttribute api/values
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
